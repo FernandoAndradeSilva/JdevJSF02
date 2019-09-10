9bb46c27817f6bd8a171f0c16a438595f1ead739
Fernando Silva <fernando.andrade.inf@gmail.com>
2019-09-10 17:35:38 -0300

    commit

--- src/main/java/br/com/cursojsf/LancamentoBean.java
+++ src/main/java/br/com/cursojsf/LancamentoBean.java
@@ -1,5 +1,18 @@
 package br.com.cursojsf;
 
-public class LancamentoBean {
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
+import br.com.dao.DaoGeneric;
+import br.com.entidades.Lancamento;
+
+@ViewScoped
+@ManagedBean(name = "lancamentoBean")
+public class LancamentoBean {
+	
+	private Lancamento lancamento = new Lancamento();
+	private DaoGeneric<Lancamento> daoGeneric = new DaoGeneric<Lancamento>();
+	
+	
+	
 }
